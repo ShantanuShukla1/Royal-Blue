1f88fe4c4248ea9db35af68848853c832b9dc486
davidkny22 <38084383+davidkny22@users.noreply.github.com>
2021-08-04 10:04:25 -0400

    random changes

--- logic.js
+++ logic.js
@@ -1,28 +1,31 @@
 var grid = [];
+var roundNumber = 1;
 for (let item of document.getElementsByClassName("grid-item")){
     item.addEventListener("click", selectItem, false);
-    grid.push(item)
+    grid.push(item);
 }
 function selectItem() {
-<<<<<<< Updated upstream
-    var user = this.textContent
-    console.log(user)
+    var user = this.textContent;
+    console.log(user);
+    this.style.setProperty("background-color", "blue");
+    computerPick(this);
 }
 
-var choseTiles = new Array();
-var score = 1;
-for (i=0, i<16, i++) {
+function computerPick(userPick) {
+    var userPickIsSameAsPick = true;
+    while (userPickIsSameAsPick) {
+        var pick = grid[Math.floor(Math.random() * grid.length)];
+        userPickIsSameAsPick = userPick == pick;
+    }
+    pick.style.setProperty("background-color", "red");
+}
+
+for (i=0; i<16; i++) {
     chosenTiles.length++;
     chosenTiles.push(1 + Math.floor(Math.random()*16));
     if(user.value == choseTiles(i)) {
         text = "You Lose";
     } else {
-        score++;
+        roundNumber++;
     }
 }
-=======
-    var user = this.textContent;
-    console.log(user);
-    this.style.background = 'blue'
-}
->>>>>>> Stashed changes
