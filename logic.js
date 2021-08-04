681ba1a892bca5ef21e474f5b25260d3ff375dc4
ShantanuShukla1 <shantanushukla006@gmail.com>
2021-08-04 10:12:36 -0400

    header

--- logic.js
+++ logic.js
@@ -1,22 +1,35 @@
 var grid = [];
+var roundNumber = 1;
 for (let item of document.getElementsByClassName("grid-item")){
     item.addEventListener("click", selectItem, false);
-    grid.push(item)
+    grid.push(item);
 }
 function selectItem() {
-    var user = this.textContent
-    console.log(user)
+    var user = this.textContent;
+    console.log(user);
+    this.style.background = 'blue';
+}
+var choseTiles = new Array();
+for (i=0;i<16; i++) {
+    this.style.setProperty("background-color", "blue");
+    computerPick(this);
 }
 
-var choseTiles = new Array();
-var score = 1;
+function computerPick(userPick) {
+    var userPickIsSameAsPick = true;
+    while (userPickIsSameAsPick) {
+        var pick = grid[Math.floor(Math.random() * grid.length)];
+        userPickIsSameAsPick = userPick == pick;
+    }
+    pick.style.setProperty("background-color", "red");
+}
+
 for (i=0; i<16; i++) {
     chosenTiles.length++;
     chosenTiles.push(1 + Math.floor(Math.random()*16));
     if(user.value == choseTiles(i)) {
         text = "You Lose";
-    }
-    else {
-        score++;
+    } else {
+        roundNumber++;
     }
 }
