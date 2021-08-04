75d97a2906d8aea93d6374f9a05891e7a0515515
ShantanuShukla1 <shantanushukla006@gmail.com>
2021-08-04 09:20:37 -0400

    change

---
+++ logic.js
@@ -0,0 +1,22 @@
+var grid = [];
+for (let item of document.getElementsByClassName("grid-item")){
+    item.addEventListener("click", selectItem, false);
+    grid.push(item)
+}
+function selectItem() {
+    var user = this.textContent
+    console.log(user)
+}
+
+var choseTiles = new Array();
+var score = 1;
+for (i=0, i<16, i++) {
+    chosenTiles.length++;
+    chosenTiles.push(1 + Math.floor(Math.random()*16));
+    if(user.value == choseTiles(i)) {
+        text = "You Lose";
+    } else {
+        score++;
+    }
+        
+}
