a9db17f3c69a4255d0568bb6e6e7c3c856f11804
davidkny22 <38084383+davidkny22@users.noreply.github.com>
2021-08-04 11:22:49 -0400

    catapulted

--- logic.js
+++ logic.js
@@ -1,30 +1,45 @@
 var grid = [];
 var roundNumber = 1;
+var score = 0;
 for (let item of document.getElementsByClassName("grid-item")){
     item.addEventListener("click", selectItem, false);
     grid.push(item);
 }
+document.getElementById("advance").addEventListener("click", advanceToNextRound, false);
 function selectItem() {
     var user = this.textContent;
     console.log(user);
     this.style.setProperty("background-color", "blue");
-    let pick = computerPick(this);
-    var result = "You Won";
-    if(pick.getAttribute("value") == this.getAttribute("value")) {
-        result = "You Lost";
+    let pickArray = computerPicks(this);
+    var userWon = true;
+    for(let pick of pickArray) {
+        if(pick.getAttribute("value") == this.getAttribute("value")) {
+            userWon = false;
+        }
     }
-    document.getElementById("result").textContent = result
+    score = score + roundNumber;
+    document.getElementById("result").textContent = score;
 }   
 
-function computerPick(userPick) {
-    let pick = null;
-    let userPickIsSameAsPick = true;
-    while (userPickIsSameAsPick) {
-        pick = grid[Math.floor(Math.random() * grid.length)];
-        userPickIsSameAsPick = userPick == pick;
+function advanceToNextRound() {
+    roundNumber++;
+    for(let gridItem of grid) {
+        gridItem.style.setProperty("background-color", "white");
     }
-    pick.style.setProperty("background-color", "red");
-    return pick;
+}
+function computerPicks(userPick) {
+    let picks = [];
+    var possibleChoices = [...grid];
+    possibleChoices.splice(grid.indexOf(userPick), 1);
+    for(var i = 0; i < roundNumber; i++) {
+       let pick = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
+       picks.push(pick);
+       possibleChoices.splice(possibleChoices.indexOf(pick), 1);
+    } 
+    for(let computerChosen of picks) {
+        computerChosen.style.setProperty("background-color", "red");
+    }
+    return picks;
 }
 
 // for (i=0; i<16; i++) {
