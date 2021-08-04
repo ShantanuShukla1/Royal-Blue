ce557e31689f5a9c7909602e29ca607296e397ad
Danielc1218 <danielc20051218@gmail.com>
2021-08-04 08:44:14 -0500

    Updated js in index to "logic.js"

--- logic.js
+++ logic.js
@@ -1,21 +1,22 @@
 var grid = [];
+var score = 1;
 for (let item of document.getElementsByClassName("grid-item")){
     item.addEventListener("click", selectItem, false);
-    grid.push(item)
+    grid.push(item);
 }
 function selectItem() {
-    var user = this.textContent
-    console.log(user)
+    var user = this.textContent;
+    console.log(user);
+    this.style.background = 'blue';
 }
-
 var choseTiles = new Array();
-var score = 1;
-for (i=0, i<16, i++) {
+for (i=0;i<16; i++) {
     chosenTiles.length++;
     chosenTiles.push(1 + Math.floor(Math.random()*16));
     if(user.value == choseTiles(i)) {
         text = "You Lose";
-    } else {
+    } 
+    else {
         score++;
     }
 }
