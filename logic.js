ef890d64c1cc1e5b1e3c391fcaf22ad6259c441e
davidkny22 <38084383+davidkny22@users.noreply.github.com>
2021-08-04 22:53:51 -0400

    updating styling and added isometric cards in correct positions

--- logic.js
+++ logic.js
@@ -58,3 +58,66 @@ function computerPicks(userPick) {
 //         roundNumber++;
 //     }
 // }
+
+// here is the code for perspective change on hover. TESTING:
+// var container = document.getElementByClass("isometric-card-holder");
+// var inner = document.getElementByClass("isometric-card");
+        
+// var onMouseEnterHandler = function(event) {
+//   update(event);
+// };
+// var onMouseLeaveHandler = function() {
+//   inner.style = "";
+// };
+// var onMouseMoveHandler = function(event) {
+//   if (isTimeToUpdate()) {
+//     update(event);
+//   }
+// };
+
+// container.onmouseenter = onMouseEnterHandler;
+// container.onmouseleave = onMouseLeaveHandler;
+// container.onmousemove = onMouseMoveHandler;
+
+// var counter = 0;
+// var updateRate = 10;
+// var isTimeToUpdate = function() {
+//   return counter++ % updateRate === 0;
+// };
+
+// var container = document.getElementByClass("isometric-card-holder");
+// var inner = document.getElementByClass("isometric-card");
+// var mouse = {
+//   _x: 0,
+//   _y: 0,
+//   x: 0,
+//   y: 0,
+//   updatePosition: function(event) {
+//     var e = event || window.event;
+//     this.x = e.clientX - this._x;
+//     this.y = (e.clientY - this._y) * -1;
+//   },
+//   setOrigin: function(e) {
+//     this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
+//     this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
+//   },
+//   show: function() { return '(' + this.x + ', ' + this.y + ')'; }
+// }
+// mouse.setOrigin(container);
+
+// var update = function(event) {
+//     mouse.updatePosition(event);
+//     updateTransformStyle(
+//       (mouse.y / inner.offsetHeight/2).toFixed(2),
+//       (mouse.x / inner.offsetWidth/2).toFixed(2)
+//     );
+//   };
+  
+//   var updateTransformStyle = function(x, y) {
+//     var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+//     inner.style.transform = style;
+//     inner.style.webkitTransform = style;
+//     inner.style.mozTransform = style;
+//     inner.style.msTransform = style;
+//     inner.style.oTransform = style;
+//   }
